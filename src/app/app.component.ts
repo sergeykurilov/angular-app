99dd47384452a59cb74f5d65cef3141baafe7edb
Sergey <sk@atomiqconsulting.com>
2020-05-06 01:25:07 +0300

    Angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,21 +7,8 @@ import {timeout} from 'rxjs/operators';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angular-app';
-  number = 2313213123213123;
-  arr = [1, 2, 3, 4, 5, 6, 7, 8];
-  obj = {
-    a: 1, b: {c: 2}
-    };
-number2 = 21313213132213123213113132;
-  img = 'https://reactjs.org/logo-og.png'
 
-  constructor() {
-    setTimeout( () => {
-        console.log('Time is over')
-        this.img = 'https://upload.wikimedia.org/wikipedia/commons/thumb/c/cf/Angular_full_color_logo.svg/1200px-Angular_full_color_logo.svg.png'
-      },5000)
-  }
+  toggle = false;
 
 
   }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import {adsComponent} from './ads/ads.component';
 import { Post2Component } from './post2/post2.component';
 import { Post3Component } from './post3/post3.component';
 import {Post4Component} from './post4/post4.component';
+import {FormsModule} from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -15,10 +16,11 @@ import {Post4Component} from './post4/post4.component';
     adsComponent,
     Post2Component,
     Post3Component,
-    Post4Component
+    Post4Component,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
